3b484d9765dca98dcba7b56fd8cfdc427b46d73f
Ekow Nii Saki Sackey <ekow.sackey@amalitech.com>
2026-01-08 16:51:14 +0000

    feat: post component. state: incomplete"
    :q
    
    exit

--- src/main/java/org/example/blogr/Controllers/HomeController.java
+++ src/main/java/org/example/blogr/Controllers/HomeController.java
@@ -1,52 +1,35 @@
 package org.example.blogr.Controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
-import org.bson.types.ObjectId;
 import org.example.blogr.Utils.ContextUtil;
 import org.example.blogr.Utils.Switcher;
-import org.example.blogr.domain.User;
-import org.example.blogr.services.UserService;
-import org.kordamp.ikonli.javafx.FontIcon;
+import org.example.blogr.domain.Post;
+import org.example.blogr.services.PostService;
+
+
+import java.util.List;
+
 
 public class HomeController {
 
-    public ImageView logo;
-    public FontIcon homeButton;
-    public FontIcon searchButton;
-    public FontIcon addButton;
-    public FontIcon profileButton;
-    private ObjectId currentUserId;
-    private User currentUser;
     @FXML public Text displayName;
-
-    private final UserService uservice = new UserService();
 
     ContextUtil context = ContextUtil.getInstance();
 
+    List<Post> allPosts;
+    private final PostService postService = new PostService();
+
     public void initialize(){
-        setCurrentUserId();
-        setCurrentUser();
+
         setDisplayName();
-    }
-
-    public ObjectId getCurrentUserId() {
-        return currentUserId;
-    }
-
-    public void setCurrentUserId() {
-        this.currentUserId = context.getCurrentUserId();
-    }
-
-    public void setCurrentUser(){
-        currentUser = uservice.getMyProfile(currentUserId);
-
+        allPosts = postService.getPosts();
+        displayPosts();
     }
 
     public void setDisplayName(){
-        displayName.setText(currentUser.username());
+        displayName.setText(context.getCurrentUser().username());
     }
 
     public void switchToHome(MouseEvent mouseEvent) {
@@ -63,5 +46,12 @@ public class HomeController {
 
     public void switchToProfile(MouseEvent mouseEvent) {
         Switcher.switchScreen(mouseEvent, Screen.PROFILE);
+    }
+
+    public void displayPosts(){
+        for (Post p: allPosts){
+            System.out.println(p);
+        }
+
     }
 }
